bb89e6901ef24b7734d1468bcf4f540dd9086ce1
Gary Johnson <gwjohnso@uvm.edu>
2014-06-12 16:50:56 -0400

    Updating project.clj for deploying to github and clojars.

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
-(defproject matrix-viz "0.1.0"
+(defproject org.clojars.lambdatronic/matrix-viz "0.1.0"
   :description "A simple library to visualize core.matrix matrices."
+  :url "https://github.com/lambdatronic/matrix-viz"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure    "1.6.0"]
