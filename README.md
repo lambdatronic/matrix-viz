875249b363416bfc45cf20db875e219de5193d9f
Gary Johnson <gwjohnso@uvm.edu>
2014-07-07 18:32:30 -0400

    Added support for both 8-bit RGB and 8-bit grayscale color ramps.

--- README.md
+++ README.md
@@ -4,13 +4,14 @@ A simple library to visualize core.matrix matrices.
 
 ## Usage
 
-(save-matrix-as-png matrix filename pixels-per-cell nodata-value)
+(save-matrix-as-png color-ramp pixels-per-cell nodata-value matrix filename)
 
-This will render the matrix as an 8-bit RGBA color image and save it
-to filename in PNG format. Any pixels matching nodata-value will be
-masked out. The size of the output image will be rows *
-pixels-per-cell by columns * pixels-per-cell, where pixels-per-cell is
-a positive integer.
+This will render the matrix as either an 8-bit grayscale image
+(color-ramp = :gray) or an 8-bit RGB image (color-ramp = :color) and
+save it to the given filename in PNG format. Any pixels matching
+nodata-value will be masked out. The size of the output image will be
+rows * pixels-per-cell by columns * pixels-per-cell, where
+pixels-per-cell is a positive integer.
 
 ## License
 
--- src/matrix_viz/core.clj
+++ src/matrix_viz/core.clj
@@ -9,11 +9,14 @@
 (m/set-current-implementation :vectorz)
 
 (defn get-cell-color
-  [^double percentage] ; [0-1]
-  (let [h (float (- 0.7 (* percentage 0.7))) ; blue to red
-        s (float 1.0)
-        b (float 1.0)]
-    (Color/getHSBColor h s b)))
+  [color-ramp ^double percentage] ; [0-1]
+  (case color-ramp
+    :color (let [h (float (- 0.7 (* percentage 0.7))) ; blue to red
+                 s (float 1.0)
+                 b (float 1.0)]
+             (Color/getHSBColor h s b))
+    :gray  (let [p (float (- 0.7 (* percentage 0.7)))]
+             (Color. p p p))))
 
 (defn fill-cell
   [^Graphics graphics2D x y pixels-per-cell color]
@@ -33,47 +36,51 @@
      (- max min)))
 
 (defn draw-matrix!
-  [^Graphics graphics2D matrix rows cols pixels-per-cell nodata-value legend-min legend-max]
+  [^Graphics graphics2D matrix rows cols pixels-per-cell nodata-value legend-min legend-max color-ramp]
   (dotimes [x cols]
     (dotimes [y rows]
       (let [cell-value (m/mget matrix y x)]
         (if-not (= nodata-value cell-value)
           (->> (normalize legend-min legend-max cell-value)
-               (get-cell-color)
+               (get-cell-color color-ramp)
                (fill-cell graphics2D x y pixels-per-cell)))))))
 
 (defn draw-legend-color-ramp!
-  [^Graphics graphics2D legend-top legend-color-height legend-width legend-padding]
+  [^Graphics graphics2D legend-top legend-color-height legend-width legend-padding color-ramp]
   (doseq [x (range legend-padding (+ legend-padding legend-width))]
-    (let [cell-color (get-cell-color (/ (- x legend-padding) legend-width))] ; ranges from [0-1]
+    (let [cell-color (get-cell-color color-ramp (/ (- x legend-padding) legend-width))] ; ranges from [0-1]
       (doseq [y (range legend-top (+ legend-top legend-color-height))]
         (fill-cell graphics2D x y 1 cell-color)))))
 
 (defn draw-legend-text!
-  [^Graphics graphics2D legend-min legend-max legend-padding image-width image-height]
+  [^Graphics graphics2D legend-min legend-max legend-padding image-width image-height color-ramp]
   (let [min-val-string (format "Min: %.2f" legend-min)
         max-val-string (format "Max: %.2f" legend-max)
         max-val-width  (.. graphics2D getFontMetrics (stringWidth max-val-string))]
     (doto graphics2D
-      (.setColor (get-cell-color 0.0))
+      (.setColor (if (= color-ramp :color)
+                   (get-cell-color color-ramp 0.0)
+                   (get-cell-color color-ramp 1.0)))
       (.drawString min-val-string
                    ^Integer legend-padding
                    ^Integer (- image-height legend-padding))
-      (.setColor (get-cell-color 1.0))
+      (.setColor (get-cell-color color-ramp 1.0))
       (.drawString max-val-string
                    ^Integer (- image-width legend-padding max-val-width)
                    ^Integer (- image-height legend-padding)))))
 
 (defn render-matrix
-  [matrix pixels-per-cell nodata-value
+  [matrix pixels-per-cell nodata-value color-ramp
    {:keys [rows cols image-height image-width legend-color-height
            legend-padding legend-top legend-width legend-min legend-max]}]
-  (let [image (BufferedImage. image-width image-height BufferedImage/TYPE_INT_ARGB)]
+  (let [image (BufferedImage. image-width image-height (if (= color-ramp :color)
+                                                         BufferedImage/TYPE_INT_RGB
+                                                         BufferedImage/TYPE_BYTE_GRAY))]
     (doto (.createGraphics image)
       (set-background-color! Color/WHITE image-width image-height)
-      (draw-matrix! matrix rows cols pixels-per-cell nodata-value legend-min legend-max)
-      (draw-legend-color-ramp! legend-top legend-color-height legend-width legend-padding)
-      (draw-legend-text! legend-min legend-max legend-padding image-width image-height)
+      (draw-matrix! matrix rows cols pixels-per-cell nodata-value legend-min legend-max color-ramp)
+      (draw-legend-color-ramp! legend-top legend-color-height legend-width legend-padding color-ramp)
+      (draw-legend-text! legend-min legend-max legend-padding image-width image-height color-ramp)
       (.dispose))
     image))
 
@@ -98,9 +105,9 @@
      :legend-max          (apply max (remove #{nodata-value} (m/eseq matrix)))}))
 
 (defn save-matrix-as-png
-  [matrix filename pixels-per-cell nodata-value]
-  {:pre [(pos? pixels-per-cell) (integer? pixels-per-cell)]}
+  [color-ramp pixels-per-cell nodata-value matrix filename]
+  {:pre [(pos? pixels-per-cell) (integer? pixels-per-cell) (contains? #{:color :gray} color-ramp)]}
   (let [^BufferedImage image (->> (make-image-parameters matrix pixels-per-cell nodata-value)
-                                  (render-matrix matrix pixels-per-cell nodata-value))]
+                                  (render-matrix matrix pixels-per-cell nodata-value color-ramp))]
     (try (ImageIO/write image "png" (io/file filename))
          (catch IOException _ (println "Failed to write matrix to PNG file" filename)))))
